067e2f8e634fc3f241cd7913da7a9d9c380b0f5a
Ochieng Paul <ochieng442@outlook.com>
2022-03-01 12:08:35 +0300

    update

--- dist/js/head.js
+++
@@ -1,8 +0,0 @@
-// navbar section display
-fetch("../../src/partials/head.html")
-    .then(response => {
-        return response.text()
-    })
-    .then(data => {
-        document.getElementById("head").innerHTML = data;
-    });
